ab4eda5b89d85dd3c7b28f885b8325a2292fdf88
wright <jeyko1@gmail.com>
2025-03-31 21:29:21 +0300

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,195 +1,221 @@
-// custom build and feature flags
-#ifdef DEBUG
-	#define OPENGL_DEBUG        0
-	#define FULLSCREEN          0
-	#define DESPERATE           0
-	#define BREAK_COMPATIBILITY 0
-#else
-	#define OPENGL_DEBUG        0
-	#define FULLSCREEN          1
-	#define DESPERATE           0
-	#define BREAK_COMPATIBILITY 0
-#endif
+#define WINDOWS_IGNORE_PACKING_MISMATCH
 
-#define ACCUMULATE 0
+#define EDITOR									1
+#define OPENGL_DEBUG						1
+#define FULLSCREEN							0
+#define VSYNC										1
+#define AUTORES									0
+
+
+#define XRES										1280
+#define YRES										720
+
+#define SONG_DURATION						380
+
+#define MUSICFB									framebuffers[0]
+#define PROG_RENDER							programs[0]
+#define PROG_POST								programs[1]
+#define PROG_MUSIC							programs[2]
+
 
 #include "definitions.h"
 #include "glext.h"
-#include "shaders/texture.inl"
-#include "shaders/fragment.inl"
-#include "shaders/post.inl"
-
 #include "timeapi.h"
+#include "shaders/all_shaders.h"
 
 #pragma data_seg(".pids")
-// static allocation saves a few bytes
-static int pidTexture;
-static int pidMain;
-static int pidPost;
-// static HDC hDC;
+	static int frame = 1; // ulong? needed?
+	static float audio_time;
+	static int programs[4];
+	static HDC hDC;
+	static GLuint ssbo;
+	//static GLuint textures[6];
+	//static GLuint framebuffers[6];
+	static int xres;
+	static int yres;
 
-#ifndef EDITOR_CONTROLS
-#pragma code_seg(".main")
-void entrypoint(void)
-#else
+#include "debug.h"
+#include "music.h"
 #include "editor.h"
-#include "song.h"
-int __cdecl main(int argc, char* argv[])
-#endif
-{
-	// initialize window
-	#if FULLSCREEN
-		ChangeDisplaySettings(&screenSettings, CDS_FULLSCREEN);
-		ShowCursor(0);
-		const HDC hDC = GetDC(CreateWindow((LPCSTR)0xC018, 0, WS_POPUP | WS_VISIBLE | WS_MAXIMIZE, 0, 0, 0, 0, 0, 0, 0, 0));
-	#else
-		#ifdef EDITOR_CONTROLS
-			HWND window = CreateWindow("static", 0, WS_POPUP | WS_VISIBLE, 0, 0, XRES, YRES, 0, 0, 0, 0);
-			HDC hDC = GetDC(window);
-		#else
-			HDC hDC = GetDC(CreateWindow((LPCSTR)0xC018, 0, WS_POPUP | WS_VISIBLE, 0, 0, XRES, YRES, 0, 0, 0, 0));
+
+
+
+
+#pragma data_seg(".screensettings")
+static DEVMODE screenSettingsb = { 
+	{0}, 0, 0, sizeof(screenSettingsb), 0, DM_PELSWIDTH|DM_PELSHEIGHT,
+	{0}, 0, 0, 0, 0, 0, {0}, 0, 0, XRES, YRES, 0, 0,
+	#if(WINVER >= 0x0400)
+		0, 0, 0, 0, 0, 0,
+			#if (WINVER >= 0x0500) || (_WIN32_WINNT >= 0x0400)
+			0, 0
 		#endif
 	#endif
+};
 
-	// initalize opengl context
+
+
+static void __forceinline init_window() {
+#if EDITOR
+    #define WINDOW_CLASS_NAME "G"
+#else
+    #define WINDOW_CLASS_NAME (LPCSTR)0xC018
+#endif
+	#if EDITOR
+
+		for(int i = 0; i < 20; i++){
+			keys_pressed[i] = false;
+		}
+		HINSTANCE hInstance =	GetModuleHandle(NULL);
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = WINDOW_CLASS_NAME;
+    
+    RegisterClass(&wc);
+		
+    int screenWidth = GetSystemMetrics(SM_CXSCREEN); 
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN); 
+
+    int windowX = (screenWidth - xres) / 2;
+    int windowY = (screenHeight - yres) / 2;
+	#endif
+	#if AUTORES
+		xres = GetSystemMetrics(SM_CXSCREEN);
+		yres = GetSystemMetrics(SM_CYSCREEN);
+	#else
+		xres = XRES; 
+		yres = YRES;
+		//xres_real = GetSystemMetrics(SM_CXSCREEN); 
+		//yres_real = GetSystemMetrics(SM_CYSCREEN);
+	#endif
+
+	PIXELFORMATDESCRIPTOR pfd = {
+		sizeof(pfd), 1, PFD_DRAW_TO_WINDOW|PFD_SUPPORT_OPENGL|PFD_DOUBLEBUFFER, PFD_TYPE_RGBA,
+		32, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 32, 0, 0, PFD_MAIN_PLANE, 0, 0, 0, 0
+	};
+
+	DEVMODE screenSettings = { 
+		{0}, 0, 0, sizeof(screenSettings), 0, DM_PELSWIDTH|DM_PELSHEIGHT,
+		{0}, 0, 0, 0, 0, 0, {0}, 0, 0, 0, 0, 0, 0,
+		#if(WINVER >= 0x0400)
+			0, 0, 0, 0, 0, 0,
+				#if (WINVER >= 0x0500) || (_WIN32_WINNT >= 0x0400)
+				0, 0
+			#endif
+		#endif
+	};
+
+	screenSettings.dmPelsWidth = xres;
+	screenSettings.dmPelsHeight = yres;
+	screenSettings.dmFields=DM_PELSWIDTH|DM_PELSHEIGHT;
+
+	#if FULLSCREEN
+		//SetProcessDPIAware();
+		//auto disp_settings = ChangeDisplaySettings(&screenSettings, CDS_FULLSCREEN);
+		auto disp_settings = ChangeDisplaySettings(&screenSettings, CDS_FULLSCREEN);
+		//if(DISP_CHANGE_SUCCESSFUL != disp_settings){
+//DISP_CHANGE_SUCCESSFUL
+		// if(disp_settings == -2){
+		// 	MessageBox(NULL, "error", "error", 0x00000000L);
+		// }
+		ShowCursor(0);
+		hDC = GetDC(CreateWindow((LPCSTR)0xC018, 0, WS_POPUP | WS_VISIBLE | WS_MAXIMIZE, 0, 0, xres, yres, 0, 0, 0, 0));
+		//hDC = GetDC(CreateWindow((LPCSTR)0xC018, 0, WS_POPUP | WS_VISIBLE | WS_MAXIMIZE, 0, 0, 0, 0, 0, 0, 0, 0));
+	#else
+		hDC = GetDC(
+			#if EDITOR
+				hwnd = 
+			#endif
+			#if EDITOR
+				CreateWindow( WINDOW_CLASS_NAME , 0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, windowX - xres/2, windowY - yres/2, xres, yres, 0, 0, hInstance, 0)
+			#else
+				CreateWindow((LPCSTR)0xC018, 0, WS_POPUP | WS_VISIBLE, 0, 0, xres, yres, 0, 0, 0, 0)
+			#endif
+		);
+	#endif
+
 	SetPixelFormat(hDC, ChoosePixelFormat(hDC, &pfd), &pfd);
 	wglMakeCurrent(hDC, wglCreateContext(hDC));
-	
-	// create and compile shader programs
-	pidTexture = ((PFNGLCREATESHADERPROGRAMVPROC)wglGetProcAddress("glCreateShaderProgramv"))(GL_FRAGMENT_SHADER, 1, &texture);
-	pidMain    = ((PFNGLCREATESHADERPROGRAMVPROC)wglGetProcAddress("glCreateShaderProgramv"))(GL_FRAGMENT_SHADER, 1, &fragment);
-	pidPost    = ((PFNGLCREATESHADERPROGRAMVPROC)wglGetProcAddress("glCreateShaderProgramv"))(GL_FRAGMENT_SHADER, 1, &post);
 
-#ifndef EDITOR_CONTROLS
+	#if OPENGL_DEBUG
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		oglDebugMessageCallback(MessageCallback, 0); 
+	#endif
+
+	#if VSYNC
+		typedef BOOL (WINAPI *PFNWGLSWAPINTERVALEXTPROC)(int interval);
+		auto wglSwapIntervalEXT = (PFNWGLSWAPINTERVALEXTPROC) wglGetProcAddress("wglSwapIntervalEXT");
+		wglSwapIntervalEXT(1);
+	#endif
+	#if EDITOR
+		CreateChildConsole();
+		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)MessageLoop, NULL, 0, NULL);
+	#endif
+
+	glDisable(GL_BLEND);
+}
+
+
+
+static int __forceinline add_program(unsigned int program_type, const char* str, const char* path) {
+	#if OPENGL_DEBUG
+		file_paths[shaders_count] = path;
+		shader_types[shaders_count] = program_type;
+		shaders_count++;
+		return oglCreateShaderProgramv(program_type, 1, &str);
+	#else
+		return oglCreateShaderProgramv(program_type, 1, &str);
+	#endif
+}
+
+#include "render.h"
+
+//int __cdecl main(int argc, char* argv[]){
+#ifdef _DEBUG
+//int WinMainCRTStartup(){
+int main(){
 #else
-	Leviathan::Editor editor = Leviathan::Editor();
-	
-	editor.compileAndDebugShader(texture, "texture", false);
-	editor.compileAndDebugShader(fragment, "fragment", false);
-	editor.compileAndDebugShader(post, "texture", false);
-	
-	editor.updateShaders(&pidMain, &pidPost, true);
+#pragma code_seg(".main")
+void entrypoint(void) {
+//#pragma code_seg(".main")
 #endif
+//int main(){
+//void entrypoint(void) {
 
-	// texture for roughness map
-	GLuint roughnessTexture = 0;
-	glGenTextures(1, &roughnessTexture);
-	glBindTexture(GL_TEXTURE_2D, roughnessTexture);
-	// parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// render the roughness map to the texture
-	((PFNGLUSEPROGRAMPROC)wglGetProcAddress("glUseProgram"))(pidTexture);
-	((PFNGLUNIFORM2FPROC)wglGetProcAddress("glUniform2f"))(0, XRES, YRES);
-	glRects(-1, -1, 1, 1);
-	glReadBuffer(GL_BACK);
-	glCopyTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 0, 0, YRES, YRES, 0);
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	
+	init_window();
+	init_shaders();
+	init_resources();
 
-	// main rendering texture (for accumulating values)
-	GLuint mainTexture = 0;
-	glGenTextures(1, &mainTexture);
-	glBindTexture(GL_TEXTURE_2D, mainTexture);
-	// parameters
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, XRES, YRES, 0, GL_RGBA, GL_FLOAT, 0);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	// framebuffer
-	GLuint framebuffer = 0;
-	((PFNGLGENFRAMEBUFFERSPROC)wglGetProcAddress("glGenFramebuffers"))(1, &framebuffer);
-	((PFNGLBINDFRAMEBUFFERPROC)wglGetProcAddress("glBindFramebuffer"))(GL_FRAMEBUFFER, framebuffer);
-	((PFNGLBINDFRAMEBUFFERPROC)wglGetProcAddress("glBindFramebuffer"))(GL_DRAW_FRAMEBUFFER, framebuffer);
-	((PFNGLFRAMEBUFFERTEXTURE2DPROC)wglGetProcAddress("glFramebufferTexture2D"))(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, mainTexture, 0);
-	
-	const GLenum attachments[] = { GL_COLOR_ATTACHMENT0 };
-	((PFNGLDRAWBUFFERSPROC)wglGetProcAddress("glDrawBuffers"))(1, attachments);
 
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+	audio_init();
 
-	// enable blending for accumulating the values over frames
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_ONE, GL_ONE);
-
-	// main loop
-	const unsigned long samples = 750;
-	unsigned long frame = 1;
-	glBindTexture(GL_TEXTURE_2D, roughnessTexture);
-
-	const int t = timeGetTime();
-	bool done = false;
-
-	do
-	{
-#if !(DESPERATE)
+	do {
+	#if EDITOR
+	  MSG msg;
+		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			if (msg.message == WM_QUIT) return 0;
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	#else
 		PeekMessage(0, 0, 0, 0, PM_REMOVE);
-#endif
-
-#if ACCUMULATE
-		// present the result immediately and render iteratively
-		// not allowed in the compo, just for debugging
-		glEnable(GL_BLEND);
-		((PFNGLBINDFRAMEBUFFERPROC)wglGetProcAddress("glBindFramebuffer"))(GL_DRAW_FRAMEBUFFER, framebuffer);
-		const GLenum attachments[] = { GL_COLOR_ATTACHMENT0 };
-		((PFNGLDRAWBUFFERSPROC)wglGetProcAddress("glDrawBuffers"))(1, attachments);
-		glBindTexture(GL_TEXTURE_2D, roughnessTexture);
-		((PFNGLUSEPROGRAMPROC)wglGetProcAddress("glUseProgram"))(pidMain);
-		((PFNGLUNIFORM1IPROC)wglGetProcAddress("glUniform1i"))(0, frame);
-		((PFNGLUNIFORM2FPROC)wglGetProcAddress("glUniform2f"))(2, XRES, YRES);
-		glRects(-1, -1, 1, 1);
-
-		frame += 1;
-
-		glBindTexture(GL_TEXTURE_2D, mainTexture);
-		((PFNGLBINDFRAMEBUFFERPROC)wglGetProcAddress("glBindFramebuffer"))(GL_DRAW_FRAMEBUFFER, 0);
-		glDrawBuffer(GL_BACK);
-		((PFNGLUSEPROGRAMPROC)wglGetProcAddress("glUseProgram"))(pidPost);
-		((PFNGLUNIFORM1IPROC)wglGetProcAddress("glUniform1i"))(0, frame);
-		((PFNGLUNIFORM2FPROC)wglGetProcAddress("glUniform2f"))(2, XRES, YRES);
-		glDisable(GL_BLEND);
-		glRects(-1, -1, 1, 1);
-#else
-		// time based accumulation
-		//if (timeGetTime()-t < 30*1000)
-		// render and accumulate N amount of samples
-		if (frame < samples)
-		{
-			((PFNGLUSEPROGRAMPROC)wglGetProcAddress("glUseProgram"))(pidMain);
-			((PFNGLUNIFORM1IPROC)wglGetProcAddress("glUniform1i"))(0, frame);
-			((PFNGLUNIFORM2FPROC)wglGetProcAddress("glUniform2f"))(2, XRES, YRES);
-			glRects(-1, -1, 1, 1);
-
-			frame += 1;
-		}
-		// after rendering the desired amount, present the result and render post processing
-		// includes averaging the samples, tonemapping and more
-		else
-		{
-			/*
-			if (!done) {
-				printf("Samples: %d\n", frame);
-				done = true;
-			}
-			*/
-
-			glBindTexture(GL_TEXTURE_2D, mainTexture);
-			((PFNGLBINDFRAMEBUFFERPROC)wglGetProcAddress("glBindFramebuffer"))(GL_DRAW_FRAMEBUFFER, 0);
-			glDrawBuffer(GL_BACK);
-			((PFNGLUSEPROGRAMPROC)wglGetProcAddress("glUseProgram"))(pidPost);
-			((PFNGLUNIFORM1IPROC)wglGetProcAddress("glUniform1i"))(0, frame);
-			((PFNGLUNIFORM2FPROC)wglGetProcAddress("glUniform2f"))(2, XRES, YRES);
-			glDisable(GL_BLEND);
-			glRects(-1, -1, 1, 1);
-		}
-#endif
-
+	#endif
+		main_loop();
+		do_editor_stuff();
 		SwapBuffers(hDC);
-	}
-	while(!GetAsyncKeyState(VK_ESCAPE));
+	} while (
+	#if OPENGL_DEBUG
+		true
+	#else
+		!GetAsyncKeyState(VK_ESCAPE) || audio_time > SONG_DURATION
+	#endif
+	);
 
+	#if EDITOR
+		FreeConsole();  // Detach the console
+	#endif
 	ExitProcess(0);
 }
--- src/shaders/post.frag
+++ src/shaders/post.frag
@@ -1,72 +1,157 @@
-#version 430
+#version 460 core
 
-layout(location = 0) uniform int iFrame;
-layout(binding = 0) uniform sampler2D imageTexture;
-layout(location = 2) uniform vec2 resolution;
+layout(location = 0) uniform int F;
+layout(location = 1) uniform float T;
+layout(location = 2) uniform vec2 R;
 
-float gamma = 2.2;
-vec3 tonemap(vec3 color){
-	float A = 0.15;
-	float B = 0.50;
-	float C = 0.10;
-	float D = 0.20;
-	float E = 0.01;
-	float F = 0.30;
-	float W = 90.0;
-	float exposure = 4.5;
-	color *= exposure;
-	color = ((color * (A * color + C * B) + D * E) / (color * (A * color + B) + D * F)) - E / F;
-	float white = ((W * (A * W + C * B) + D * E) / (W * (A * W + B) + D * F)) - E / F;
-	color /= white;
-	
-	return color;
+layout(std430,binding=0) coherent buffer Aa{uint hist[];};
+
+out vec4 C;
+
+uint seed = 111425u;
+uint hash_u(uint _a) {
+	uint a = _a;
+	a ^= a >> 16;
+	a *= 0x7feb352du;
+	a ^= a >> 15;
+	a *= 0x846ca68bu;
+	a ^= a >> 16;
+	return a;
 }
 
-const int N = 8;
-vec3 ca(sampler2D t, vec2 UV){
-	vec2 uv = 1.0 - 2.0 * UV;
-	vec3 c = vec3(0);
-	float rf = 1.0;
-	float gf = 1.0;
-    float bf = 1.0;
-	float f = 1.0/float(N);
-	for(int i = 0; i < N; ++i){
-		c.r += f*texture(t, 0.5-0.5*(uv*rf) ).r;
-		c.g += f*texture(t, 0.5-0.5*(uv*gf) ).g;
-		c.b += f*texture(t, 0.5-0.5*(uv*bf) ).b;
-		bf *= 0.9995;
-		gf *= 0.9997;
-        rf *= 0.99988;
+float hash_f_s(float s){ return ( float( hash_u(uint(s)) ) / float( -1u ) ); }
+float hash_f(){ uint s = hash_u(seed); seed = s;return ( float( s ) / float( -1u ) ); }
+vec2 hash_v2(){ return vec2(hash_f(), hash_f()); }
+vec3 hash_v3_s(float s){ return vec3(hash_f_s(s), hash_f_s(s), hash_f_s(s)); }
+vec3 hash_v3(){ return vec3(hash_f(), hash_f(), hash_f()); }
+
+uint get_hist_id(ivec2 c){
+	return (c.x + uint(R.x) * c.y + uint(R.x*R.y))%uint(R.x*R.y);
+}
+
+
+const int COL_CNT = 4;
+vec3[4] kCols = vec3[](
+	vec3(1.,1,1), vec3(1.,0.,1.),
+	vec3(1,1.,0.1)*1., vec3(0.5,1,1.)*1.5
+);
+
+vec3 mix_cols(float idx){
+	//idx = mod(idx,1.);
+	int cols_idx = int(idx*float(COL_CNT));
+	float fract_idx = fract(idx*float(COL_CNT));
+	fract_idx = smoothstep(0.,1.,fract_idx);
+	//return oklab_mix( kCols[cols_idx], kCols[(cols_idx + 1)%COL_CNT], fract_idx );
+	return mix( kCols[cols_idx], kCols[(cols_idx + 1)%COL_CNT], fract_idx );
+}
+
+
+
+void main( ){
+	vec2 uv = gl_FragCoord.xy/R.xy;
+
+	seed = uint(gl_FragCoord.x +gl_FragCoord.y*1111);
+	hash_f();
+	//seed = uint(gl_FragCoord.x +gl_FragCoord.y*5214.);
+
+	float end = smoothstep(250.,250.1,T);
+	float endb = smoothstep(280.,280.1,T);
+
+	float repd = 0.5 + sin(T*0.5)*2./T;
+	float eenv = mod(T,repd);
+	eenv = exp(-eenv*40.);
+
+	float K_COL = 0.03
+		+ 0.1* smoothstep(90.,190.,T)
+		- end + 10.*endb * smoothstep(4.,0.,mod(T,4.));
+	;
+	float K_GLITCH = 0.;
+	K_GLITCH += 
+		smoothstep(0.,50.,T);
+	K_GLITCH *= pow(hash_f_s(floor(T)), 2);
+	K_GLITCH + 0.9* smoothstep(160.,190.,T)*eenv;
+	K_GLITCH *= 1.-end + endb;
+
+
+
+	if(T > 80 && T < 100.){
+		//float env = exp(-mod(T,4.));
+		float env = smoothstep(1.,0.,mod(T,4.));
+        //float repd = 0.5 + sin(t*0.5)*2./t;
+		K_GLITCH = 100. * env;
 	}
-	return c;
+
+	uint hist_id =  get_hist_id(ivec2(gl_FragCoord.xy));
+
+	// tonemap
+	vec3 col = vec3(hist[hist_id]) * 0.0001;
+	vec3 pal = mix_cols(mod(col.x*115.,1.));
+	col = col/(1.+col);
+	if(hash_f_s(floor(T*0.8))<0.7){
+	//if(true){
+		col = 1.- col;
+	} else {
+		col = pow(step(col,vec3(.5)),vec3(0.02));
+		col = pow(abs(col),vec3(0.02));
+	}
+
+	if(T > 60 && T < 65.){
+		col = 1. - col;
+		//env = exp(-(T-60.))*4.;
+	}
+
+	if(abs(col.x - 0.1) < K_COL
+	//&& hash_f_s(floor(T)) < 1
+	){
+		col -= pal;
+	}
+
+	C = vec4(pow(abs(col), vec3(2./0.45454545)),1);
+
+	if(mod(T,9.- endb*5.) < 1.){
+		hist[hist_id] = 0;
+	} else {
+		if (col.x < 0.0 + K_GLITCH){
+			ivec2 offs = -ivec2(0, 4);
+			//seed += iFrame;
+			offs *= ((int(hash_f() < col.x*1111.))*2 - 1)*(1 + 5*int(hash_f_s(floor(T))));
+			offs *= int(hash_f_s(floor(T) * 124.)*2.)*2 - 1;
+			offs *= 1 + int(endb * smoothstep(3.,0.,mod(T,4.))*20.);
+			if(T > 122){
+				offs = ivec2(offs.y,offs.x);
+			}
+			if(T > 316 && hash_f() > 0.5){
+				offs = ivec2(offs.y,offs.x)/10;
+			}
+			
+			hist[hist_id] =	hist[get_hist_id((ivec2(gl_FragCoord.xy) + offs))];
+		}else {
+			hist[hist_id] = 0;
+		}
+	}
+	if(end > 0.1){
+		//C*= 0.1;
+		C = 1.-C;
+	}
+//    fragColor = vec4(1,0,0,1);    
+	//fragColor = texture(tex_music, uv);
+//	fragColor = imageLoad(img_music, ivec2(gl_FragCoord.xy));
+
+
+	//asdgsdagasdg
+
+	//asdgasdg
+	//asdg
+	//asdgasdg
+
+	//oasdgasdg
+
+	//fragColor *= 0.000001;
+	//asdg
+
+	//fragColor *= fract(T);
+
+	
 }
 
-vec3 source(vec2 coord){
-    //vec4 fc = texelFetch(imageTexture, ivec2(coord), 0)/float(iFrame);
-    vec3 fc = ca(imageTexture, coord/resolution.xy)/float(iFrame);
-    
-    // vignette
-    vec2 uv = (coord / resolution.xy - 0.5) * (resolution.x/resolution.y) * 2.0;
-	fc.rgb *= 1.0 / pow(0.09 * dot(uv, uv) + 1.0, 2.0);
 
-    fc.rgb = tonemap(fc.rgb);
-    fc.rgb = smoothstep(vec3(0), vec3(1), fc.rgb);
-    return fc.rgb;
-}
-
-void main(){
-    vec3 center = source(gl_FragCoord.xy + vec2( 0, 0));
-    vec3 c = vec3(0);
-    //c += source(fragCoord + vec2(-1,-1));
-    //c += source(fragCoord + vec2( 1,-1));
-    //c += source(fragCoord + vec2(-1, 1));
-    //c += source(fragCoord + vec2( 1, 1));
-    c += -1.0*source(gl_FragCoord.xy + vec2( 0,-1));
-    c += -1.0*source(gl_FragCoord.xy + vec2(-1, 0));
-    c +=  5.0*center;
-    c += -1.0*source(gl_FragCoord.xy + vec2( 1, 0));
-    c += -1.0*source(gl_FragCoord.xy + vec2( 0, 1));
-    
-    c = mix(center, c, 0.12);
-    gl_FragColor.rgb = pow( pow(c, vec3(1.005, 1.0, 0.99)), vec3(1.0 / gamma));
-}
